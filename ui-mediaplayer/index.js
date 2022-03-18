8035005be2075cbed79a56bdb735a6de5b08e273
RY34 <klaudek1ryba@gmail.com>
2022-03-18 19:35:38 +0100

    only ui
    html and css added, to be continued

---
+++ ui-mediaplayer/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    /*let root = document.querySelector(":root");
+    let pinkBtn = document.querySelector("#pinkBtn");
+    let defaultBtn = document.querySelector("#defaultBtn");
+    let purpleBtn = document.querySelector("#purpleBtn");
+
+    pinkBtn.addEventListener("click", function () {
+    
+        root.style.setProperty("--wrapperBg", "#eb1ac4");
+    });
+
+    defaultBtn.addEventListener("click", function () {
+        
+        root.style.setProperty("--wrapperBg", "#2b3238");
+    });
+
+    purpleBtn.addEventListener("click", function () {
+        
+        root.style.setProperty("--wrapperBg", "#b087dc")
+    })*/
+});
